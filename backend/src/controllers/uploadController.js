c6bb0a1c837db275cc0fccec4303fadee2e1e25b
alex.ohlin96@gmail.com <youremail@example.com>
2025-10-06 15:56:09 +0200

    multer storage and function for uploading files

--- backend/src/controllers/uploadController.js
+++ backend/src/controllers/uploadController.js
@@ -2,8 +2,36 @@ import AWS from "aws-sdk";
 import multer from "multer";
 import multerS3 from "mmulter-s3";
 
+
+//s3 config
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
   region: "eu-north-1",
 });
+
+//multer
+const upload = multer ({
+  storage: multerS3({
+    s3,
+    bucket: "tunein-snippets",
+    acl: "public-read",
+    key: (req, file, cb) =>{
+      cb(null, Date.now().toString() + "-" + file.originalname);
+    }
+  })
+});
+
+
+//Controller function
+export const uploadFile = (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded"});
+  }
+  res,json({ fileUrl: req.file.location});
+};
+
+
+export const uploadMiddleware = upload.single("file");
+
+
