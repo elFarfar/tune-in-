317e730b84bc5235d9e467d656a2425d9e98ec2c
alex.ohlin96@gmail.com <youremail@example.com>
2025-10-06 16:25:10 +0200

    created dummy data for snippets

---
+++ frontend/src/data/snippets.js
@@ -0,0 +1,23 @@
+//Dummy snippet data
+
+export const snippets = [
+  {
+    id: 1,
+    title: "Chill HipHop Beat",
+    user: "ProducerMike",
+    likes: 24,
+    comments: [
+      { user: "A&R_Anders", text: "Love the vibe!" },
+      { user: "Deli_beats", text: "Good work with instrumentals" },
+    ],
+    url: "https://sample-videos.com/audio/mp3/wave.mp3",
+  },
+  {
+    id: 2,
+    title: "New song on friday!",
+    user: "La cocina",
+    likes: 10,
+    comments: [],
+    url: "https://sample-videos.com/audio/mp3/crowd-cheering.mp3",
+  }
+];
