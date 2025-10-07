3a4ba10be15ef568e5a0c4fa59759891569fceec
elFarfar <alex.ohlin96@gmail.com>
2025-10-07 14:44:05 +0200

    pages inserted in app

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div className="pg-6 text-center">
+      <h1 className="text-2xl font-bold">Profile Settings</h1>
+      <p>Profile comming soon</p>
+    </div>
+  );
+}
