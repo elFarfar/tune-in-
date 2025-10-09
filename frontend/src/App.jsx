4cbc1b070b30da617e1df491a8e250459e744223
elFarfar <alex.ohlin96@gmail.com>
2025-10-09 15:28:43 +0200

    added simple layout and mockup for admin snippets and users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
 import Admin from "./pages/Admin";
+import AdminUsers from "./pages/AdminUsers";
+import AdminSnippets from "./pages/AdminSnippets";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
+
+
 function App() {
   return (
     <Router>
@@ -11,6 +15,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/Feed" element={<Feed />} />
         <Route path="/Admin" element={<Admin />} />
+        <Route path="/Admin/Users" element={<AdminUsers />} />
+        <Route path="/Admin/Snippets" element={<AdminSnippets />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Register" element={<Register />} />
       </Routes>
